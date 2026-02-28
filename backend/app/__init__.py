f7e22d3ed5b6b38be061e16476a94e602da3e8be
JohnSang16 <johnsang1970@gmail.com>
2026-02-28 02:02:38 -0500

    chore: created and init code for config.py and extensions.py, also connected the two via __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+from app.config import Config
+from app.extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    
     CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.api.routes import api_bp
     app.register_blueprint(api_bp, url_prefix="/api")
