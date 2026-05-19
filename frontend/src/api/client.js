4bc4af78536f970b74e7b9fb27175e0cdb60cc49
John Sang <johnsang1970@gmail.com>
2026-05-18 20:05:09 -0400

    chore(api): update baseURL from Railway to AWS Elastic Beanstalk

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-  baseURL: "https://pitchpulse-ai-production.up.railway.app/api",
+  baseURL: "http://pitchpulse-backend-env.eba-yhtgfwu8.us-east-1.elasticbeanstalk.com/api",
   headers: { "Content-Type": "application/json" }
 })
 
